b7acb435b8feda38975cfc4a4267e0c7f9ac3754
bryanltobing <bryantobing0@gmail.com>
2024-04-22 21:26:22 +0800

    feat: disable closing sign in

--- app/(app)/(root)/_layout.tsx
+++ app/(app)/(root)/_layout.tsx
@@ -22,11 +22,7 @@ export default function AppLayout() {
   if (!session) {
     // On web, static rendering will stop here as the user is not authenticated
     // in the headless Node process that the pages are rendered in.
-    return (
-      <>
-        <Repush href="/sign-in" />
-      </>
-    );
+    return <Repush href="/sign-in" />;
   }
 
   // This layout can be deferred because it's not the root layout.
--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -13,7 +13,6 @@ export default function AppLayout() {
         options={{
           presentation: "modal",
           gestureEnabled: false,
-          gestureDirection: "horizontal",
         }}
       />
     </Stack>
